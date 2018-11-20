932a011104e0222252f8f907e5885bfa818f7181
yaybarbs <barbs.boechat@gmail.com>
2018-11-19 22:50:07 -0200

    ATTs

--- js/main.js
+++ js/main.js
@@ -119,26 +119,7 @@
     }
 
 
-    // Get the modal do TEXTO 2
-    var modal2 = document.getElementById('myModal2');
-
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img2 = document.getElementById('myImg2');
-    var modalImg2 = document.getElementById("img02");
-    var captionText2 = document.getElementById("caption2");
-    img2.onclick = function(){
-        modal2.style.display = "block";
-        modalImg2.src = this.src;
-        captionText2.innerHTML = this.alt;
-    }
-
-    // Get the <span> element that closes the modal
-    var span2 = document.getElementsByClassName("close2")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span2.onclick = function() {
-      modal2.style.display = "none";
-    }
+    
 
     // Get the modal do TEXTO 3
     var modal3 = document.getElementById('myModal3');
